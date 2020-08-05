8c9221f8d30031a72b33c5c0765fec2e7edf909c
evoosa <eevvoossaa@gmail.com>
2020-08-05 14:00:11 +0300

    refractored dummy client

--- src/file_handler/dummy_client.py
+++ src/file_handler/dummy_client.py
@@ -1,3 +1,15 @@
 import requests
 
-res = requests.post('http://localhost:80', json={"filenames": ["file1", "file2"]})
+filenames1 = {"filenames": ['10.25.196.3_traffic.19-10-23-21-00.cdr',
+                            '10.25.196.3_traffic.19-10-23-21-01.cdr',
+                            '10.25.196.3_traffic.19-10-23-21-02.cdr',
+                            '10.25.196.3_traffic.19-10-23-21-03.cdr',
+                            '10.25.196.3_traffic.19-10-23-21-04.cdr',
+                            '10.25.196.3_traffic.19-10-23-21-05.cdr']}
+
+filenames2 = {"filenames": ['malauncher.log.1',
+                            'malauncher.log.10',
+                            'malauncher.log.11',
+                            'malauncher.log.2']}
+
+res = requests.post('http://localhost:80', json=filenames2)
